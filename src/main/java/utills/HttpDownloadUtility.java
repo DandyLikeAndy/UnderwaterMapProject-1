90027becc6507f09326e7a7338cf6f533c7d8ae8
slonikmak <slonikmak@gmail.com>
2017-05-16 00:33:23 +0300

    new

--- src/main/java/utills/HttpDownloadUtility.java
+++ src/main/java/utills/HttpDownloadUtility.java
@@ -6,12 +6,26 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by User on 15.05.2017.
  */
 public class HttpDownloadUtility {
     private static final int BUFFER_SIZE = 4096;
+    private static final String dir = "tiles";
+
+    public static void main(String[] args) throws IOException {
+        List<String> list = new ArrayList<>();
+        list.add("https://a.tile.opentopomap.org/10/517/377.png");
+        list.add("https://a.tile.opentopomap.org/10/514/377.png");
+
+        loadTiles(list);
+    }
 
     /**
      * Downloads a file from a URL
@@ -21,6 +35,7 @@ public class HttpDownloadUtility {
      */
     public static void downloadFile(String fileURL, String saveDir)
             throws IOException {
+        System.out.println("load to "+saveDir);
         URL url = new URL(fileURL);
         HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
         int responseCode = httpConn.getResponseCode();
@@ -71,5 +86,42 @@ public class HttpDownloadUtility {
             System.out.println("No file to download. Server replied HTTP code: " + responseCode);
         }
         httpConn.disconnect();
+    }
+
+    public static void loadTiles(List<String> urls) throws IOException {
+        Path path = Paths.get(dir);
+        if (!Files.exists(path)){
+            Files.createDirectory(path);
+        }
+
+        urls.forEach(url -> {
+            url.replace("http://", "");
+            String[] arr = url.split("/");
+            String z = arr[2];
+            String x = arr[3];
+            String y = arr[4];
+
+            Path zPath = path.resolve(z);
+            if (!Files.exists(zPath)) try {
+                Files.createDirectory(zPath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Path xPath = zPath.resolve(x);
+            if (!Files.exists(xPath)) try {
+                Files.createDirectory(xPath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //Path yPath = xPath.resolve(y);
+
+            try {
+                downloadFile(url, xPath.toAbsolutePath().toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
